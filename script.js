0726d4cf0d95a917ccbf57160c3c021744828e93
vasif <vasifm1@gmail.com>
2021-07-03 10:19:28 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,6 @@
+let displayNumber=(num)=>inpt.value+=num
+
+let evaluateExpression=()=>inpt.value=eval(inpt.value)
+
+let clearBox=()=>inpt.value=""
+let removeNumber=()=>inpt.value=inpt.value.slice(0,-1);
